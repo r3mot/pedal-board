971cd8ca2080aadd17d7d61cee5d8d620bf0fb03
r3mot <cmendrella1@toromail.csudh.edu>
2023-05-10 23:06:18 -0700

    refactor styles

--- src/components/pedalBoard/PedalBoard.tsx
+++ src/components/pedalBoard/PedalBoard.tsx
@@ -6,17 +6,20 @@ import {
   DistortionPedal,
 } from "../pedals";
 import { Dropdown } from "../dropdown";
+import { Record } from "../record";
 
 export const PedalBoard = () => {
   return (
-    <div className="pedal-board">
-      <Dropdown />
-      <div className="pedal-board-components">
+    <section className='pedal-board'>
+      <div className='options'>
+        <Dropdown />
+      </div>
+      <div className='pedal-board-components'>
         <ReverbPedal />
         <DelayPedal />
         <DistortionPedal />
         <VolumePedal />
       </div>
-    </div>
+    </section>
   );
 };
